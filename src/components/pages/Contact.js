ed50d574d9792a80f945a8d53555f4aebedcbf8a
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-06 20:56:43 +0000

    Removed React dom router dependencies

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a a href="mailto:metodieva.desislava@gmail.com" className="text-indigo-400 leading-relaxed">
                 metodieva.desislava@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
