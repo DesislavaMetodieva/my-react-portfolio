b90d055defd89382f4f10bb0c22c9cc3a1ba714e
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-06 10:47:58 +0000

    Added initial component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import Header from './components/pages/Header';
+import Footer from './components/pages/Footer';
+import Navbar from './components/pages/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/projects/*" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Header>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
