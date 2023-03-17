8829324bdac7c79f1e681d5e45969fe5aa8fa949
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-17 18:02:47 +0000

    Added footer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/pages/Header';
+import Footer from './components/pages/Footer'; 
 // import Contact from './components/pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -27,6 +28,7 @@ ReactDOM.render(
         */}
       </Routes>
       </Wrapper>
+      <Footer />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
