1da5a0d6c430ac3df501ce86e33e7056eeb6d7ac
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-19 23:36:08 +0000

    Added responsive layout

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -18,23 +18,23 @@ const Contact = () => {
     <div className="container mt-5">
       <h2 className="mb-3">Leave Your Feedback </h2>
       <form onSubmit={onSubmit}>
-        <div className="mb-3 w-25">
+        <div className="mb-3 w-25 smallScreen">
           <label className="form-label w-25" htmlFor="name">
             Full Name
           </label>
           <input className="form-control" type="text" id="name" required placeholder="Full Name"/>
         </div>
-        <div className="mb-3 w-25">
+        <div className="mb-3 w-25 smallScreen">
           <label className="form-label" htmlFor="email">
             Email
           </label>
           <input className="form-control" type="email" id="email" required placeholder="Email"/>
         </div>
-        <div className="mb-3 w-50">
+        <div className="mb-3 w-50 smallScreen">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" rows={8} required placeholder="Enter your message"/>
+          <textarea className="form-control smallScreen" id="message" rows={8} required placeholder="Enter your message"/>
         </div>
         <button className="button-custom border-0 py-2 px-6 rounded text-lg inline-flex font-semibold" type="submit">
           {formStatus}
